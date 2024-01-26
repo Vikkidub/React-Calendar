78d1cc6764f09af4ef26e56de93cc80d49b6a6d9
Viktor Stokstad <141615732+Vikkidub@users.noreply.github.com>
2024-01-26 11:21:20 +0100

    Initial commit

---
+++ calendar-react-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PrintTable from './table';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Welcome</h1>
+    <table> <PrintTable /> </table>
+    </div>
+  );
+}
+
+export default App;
---
+++ calendar-react-app/src/table.js
@@ -0,0 +1,21 @@
+import './table.css';
+
+function PrintTable() {
+    var rows = 5; 
+    var cols = 7; 
+    var table = [];
+
+    for (var i = 0; i < rows; i++) {
+        var cells = [];
+
+        for (var j = 1; j <= cols; j++) {
+            cells.push(<td key={j}>{i * cols + j}</td>);
+        }
+
+        table.push(<tr key={i}>{cells}</tr>);
+    }
+    
+    return table;
+}
+
+export default PrintTable;
