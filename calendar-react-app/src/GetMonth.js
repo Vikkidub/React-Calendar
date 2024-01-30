1784710ea1b6af930db9ca1ed023d7d1b6695ef1
Viktor Stokstad <141615732+Vikkidub@users.noreply.github.com>
2024-01-30 11:24:45 +0100

    Now gets the updated month pulled from the user browser

--- calendar-react-app/src/GetMonth.js
+++ calendar-react-app/src/GetMonth.js
@@ -1,5 +1,12 @@
-function getMonth(){
-    return <div>January</div>
+function getMonth() {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    
+    const time = new Date();
+    let monthIndex = time.getMonth();
+    let monthName = months[monthIndex];
+
+    return <div>{monthName}</div>;
 }
+
 
 export default getMonth;
